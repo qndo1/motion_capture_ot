35a5f72d3676e2bb5c8385eaf9847c80e701bc17
Matt Tokunaga <matttokunaga1@gmail.com>
2026-01-21 18:11:26 -0800

    added adjusted accuracy function as well as average region-level distance metric

--- utils.py
+++ utils.py
@@ -7,6 +7,51 @@ import plotly.graph_objects as go
 import pandas as pd
 import json
 
+# Some constants I don't want to have to constantly redefine
+
+import numpy as np
+
+LABELS = [
+    "head",
+    "left_foot",
+    "left_forearm",
+    "left_hand",
+    "left_shin",
+    "left_thigh",
+    "left_upper_arm",
+    "lower_torso",
+    "pelvis",
+    "right_foot",
+    "right_forearm",
+    "right_hand",
+    "right_shin",
+    "right_thigh",
+    "right_upper_arm",
+    "upper_torso",
+]
+
+LABEL_TO_INDEX = {label: i for i, label in enumerate(LABELS)}
+
+D = np.array([
+    [0, 6, 4, 5, 6, 5, 3, 2, 3, 6, 4, 5, 6, 5, 3, 1],
+    [6, 0, 6, 7, 1, 2, 4, 3, 2, 4, 6, 7, 3, 2, 4, 4],
+    [4, 6, 0, 1, 6, 5, 1, 4, 5, 6, 4, 5, 6, 5, 3, 2],
+    [5, 7, 1, 0, 7, 6, 2, 5, 6, 7, 5, 6, 7, 6, 4, 3],
+    [6, 1, 6, 7, 0, 1, 5, 4, 3, 5, 7, 8, 4, 3, 5, 5],
+    [5, 2, 5, 6, 1, 0, 4, 3, 2, 4, 6, 7, 3, 2, 4, 4],
+    [3, 4, 1, 2, 5, 4, 0, 3, 4, 5, 3, 4, 5, 4, 2, 1],
+    [2, 3, 4, 5, 4, 3, 3, 0, 1, 3, 5, 6, 4, 3, 3, 1],
+    [3, 2, 5, 6, 3, 2, 4, 1, 0, 2, 6, 7, 3, 2, 4, 2],
+    [6, 4, 6, 7, 5, 4, 5, 3, 2, 0, 6, 7, 1, 2, 4, 4],
+    [4, 6, 4, 5, 7, 6, 3, 5, 6, 6, 0, 1, 6, 5, 1, 2],
+    [5, 7, 5, 6, 8, 7, 4, 6, 7, 7, 1, 0, 7, 6, 2, 3],
+    [6, 3, 6, 7, 4, 3, 5, 4, 3, 1, 6, 7, 0, 1, 5, 5],
+    [5, 2, 5, 6, 3, 2, 4, 3, 2, 2, 5, 6, 1, 0, 4, 4],
+    [3, 4, 3, 4, 5, 4, 2, 3, 4, 4, 1, 2, 5, 4, 0, 1],
+    [1, 4, 2, 3, 5, 4, 1, 1, 2, 4, 2, 3, 5, 4, 1, 0],
+], dtype=np.int8)
+
+
 def smplx_vertices_from_amass(
     smplx_model_path,
     amass_npz_path,
@@ -156,10 +201,9 @@ def meshes_from_path(path):
 def sampled_verts_from_mesh(mesh, n_points = 1_000, return_faces = False):
     points, face_indices = trimesh.sample.sample_surface(mesh, n_points)
     
-    output = [points]
     if return_faces:
-        output.append(face_indices)
-    return tuple(output)
+        return (points, face_indices)
+    return points
 
 def sampled_verts_from_path(amass_npz_path, idx = None, n_points = 1_000, return_mesh = False, return_faces = False):
     
@@ -407,6 +451,9 @@ def face_idx_pairs_to_region_pairs(idx_pairs):
     return zip(start_regions, matched_regions)
 
 def region_accuracy(G, faces1, faces2):
+    """
+    Number of correct matchings / number of points
+    """
     regions1 = faces_to_regions(faces1)
     regions2 = faces_to_regions(((G / G.max()) @ faces2).astype(int))
     correct = 0
@@ -414,6 +461,40 @@ def region_accuracy(G, faces1, faces2):
         if regions1[i] == regions2[i]:
             correct += 1
     return correct / len(regions1)
+
+def region_accuracy_adjusted(G, faces1, faces2):
+    regions1 = faces_to_regions(faces1)
+    regions2 = faces_to_regions(((G / G.max()) @ faces2).astype(int))
+    region_counts1 = np.unique(regions1, return_counts = True)
+    region_counts2 = np.unique(regions2, return_counts = True)
+
+    if region_counts1[0].size != region_counts2[0].size:
+        raise ValueError("Not enough regions. Try sampling more points so that each region is sampled")
+    else:
+        possible_correct = 0
+        for i in range(region_counts1[0].size):
+            possible_correct += min(
+                region_counts1[1][i], region_counts2[1][i]
+            )
+    
+    correct = 0
+    for i in range(len(regions1)):
+        if regions1[i] == regions2[i]:
+            correct += 1
+    return correct / possible_correct
+
+def region_distance(label1, label2):
+    return D[LABEL_TO_INDEX[label1], LABEL_TO_INDEX[label2]]
+
+def average_region_distance(G, faces1, faces2):
+    regions1 = faces_to_regions(faces1)
+    regions2 = faces_to_regions(((G / G.max()) @ faces2).astype(int))
+
+    idx1 = np.fromiter((LABEL_TO_INDEX[r] for r in regions1), dtype=np.int64)
+    idx2 = np.fromiter((LABEL_TO_INDEX[r] for r in regions2), dtype=np.int64)
+
+    return D[idx1, idx2].mean()
+
 
 
 def plot_3d_points_and_connections(points1, points2, G, switch_yz = False, color_incorrect = False, width = 600, height = 1000):
